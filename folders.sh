ec89ae373389fbb12cf2493fe486730b922cb108
Valeria K <laaurora.val@gmail.com>
2016-07-31 02:56:41 +0300

    functional save script with sourced folders.sh

---
+++ folders.sh
@@ -0,0 +1,25 @@
+# Create labels for the subfolder structure within the collection root.
+#
+# Initial example accepts a single letter (must be unique) OR a full name 
+# spelled in any combination of uppercase and lowercase letters.
+#   a ) imgFolder="/Art/" ;;
+#   art ) imgFolder="/Art/" ;;
+#   Br ) imgFolder="/Brand/" ;;
+# etc would be eqally acceptable but would require a more precise input.
+#
+# "Misc" is where images go if no label was provided or provided label does not exist.
+
+case $1 in
+  [aA] | [aA][rR][tT] ) imgFolder="/Art/" ;;
+  [bB] | [bB][rR][aA][nN][dD] ) imgFolder="/Brand/" ;;
+  [cC] | [cC][oO][lL][oO][rR] ) imgFolder="/Color/" ;;
+  [fF] | [fF][oO][nN][tT][sS] ) imgFolder="/Fonts/" ;;
+  [hH] | [hH][oO][mM][eE] ) imgFolder="/Home/" ;;
+  [iI] | [iI][lL][lL][uU][sS][tT][rR][aA][tT][iI][oO][nN] ) imgFolder="/Illustration/" ;;
+  [pP] | [pP][hH][oO][tT][oO] ) imgFolder="/Photo/" ;;
+  [sS] | [sS][tT][yY][lL][eE] ) imgFolder="/Style/" ;;
+  [wW] | [wW][eE][bB] ) imgFolder="/Web/"  ;;
+  [mM] | [mM][iI][sS][cC] ) imgFolder="/Misc/" ;;
+  * ) imgFolder="/Misc/"  
+      osascript -e 'display notification "Image saved to a Misc folder" with title "Non-existing label"' ;;
+esac
--- img-save.sh
+++ img-save.sh
@@ -1,32 +1,59 @@
-imgRootDir=$HOME/Pictures/Images
+#!/bin/bash
+
+# set root directory for the image collection
+imgRootDir="${HOME}/Pictures/Images"
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+. "$DIR/folders.sh"
+
+# store the URL of the image contained in the clipboard
 imgSrcURL=$(pbpaste)
-imgExt=".${imgSrcURL##*.}"
-imgDate=$(date '+%s')
+
+# get the URL of the frontmost window/tab in Safari
 imgPageURL=$(osascript -e 'tell application "Safari" to set theURL to URL of front document')
 imgPageTitle=$(osascript -e 'tell application "Safari" to set theTitle to name of front document')
-imgName="${imgPageTitle} ${imgDate}${imgExt}"
-imgTags=$2
-imgComment="title: ${imgPageTitle}"$'\n\n'"page: ${imgPageURL}"$'\n\n'"url: ${imgSrcURL}"
+imgExt=".${imgSrcURL##*.}"
+imgDate=$(date '+%s')
+imgTags="$2"
+imgRes="$3"
 
-case $1 in
-  [aA] | [aA][rR][tT] ) imgFolder="/Art/" ;;
-  [bB] | [bB][rR][aA][nN][dD] ) imgFolder="/Brand/" ;;
-  [cC] | [cC][oO][lL][oO][rR] ) imgFolder="/Color/" ;;
-  [fF] | [fF][oO][nN][tT][sS] ) imgFolder="/Fonts/" ;;
-  [hH] | [hH][oO][mM][eE] ) imgFolder="/Home/" ;;
-  [iI] | [iI][lL][lL][uU][sS][tT][rR][aA][tT][iI][oO][nN] ) imgFolder="/Illustration/" ;;
-  [pP] | [pP][hH][oO][tT][oO] ) imgFolder="/Photo/" ;;
-  [sS] | [sS][tT][yY][lL][eE] ) imgFolder="/Style/" ;;
-  [wW] | [wW][eE][bB] ) imgFolder="/Web/"  ;;
-  [mM] | [mM][iI][sS][cC] ) imgFolder="/Misc/" ;;
-  * ) imgFolder="/Misc/"  
-      osascript -e 'display notification "Image saved to a Misc folder" with title "Wrong label"' ;;
-esac
+# if arg3 or arg4 are longer than 3 chars, use them for the title
+# otherwise, use the title of the frontmost page in Safari
+if [[ ${#3} -gt 3 ]]; then
+  imgTitle="$3"
+elif [[ ${#4} -gt 3 ]]; then
+  imgTitle="$4"
+else 
+  imgTitle="${imgPageTitle}"
+fi
+
+# retina handling
+if [[ "$3" == [rR] || "$3" == "2" || "$3" == "@2x" || "$4" == [rR] || "$4" == "2" || "$4" == "@2x" ]]; then
+  imgName="${imgTitle} ${imgDate} @2x${imgExt}"
+else 
+  imgName="${imgTitle} ${imgDate}${imgExt}"
+fi
+
+imgComment="title: ${imgPageTitle}"$'\n\n'"page: ${imgPageURL}"$'\n\n'"url: ${imgSrcURL}"
 
 imgPath=$imgRootDir$imgFolder$imgName
 
+# download to a chosen folder with an assigned name
 curl -o "${imgPath}" "${imgSrcURL}" && osascript -e 'display notification "done" with title "Yep!"' || osascript -e 'display notification "Done" with title "Nope!"'
 
-tag -a "${imgTags}" "${imgPath}"
+# image optimization
+if [[ "$imgExt" == .[jJ][pP]*[gG] ]]; then
+  jpegtran -optimize -progressive -outfile "${imgPath}" "${imgPath}" || \
+  osascript -e 'display notification "Something went wrong" with title "Optimisation failed"'
+elif [[ "$imgExt" == .[pP][nN][gG] ]]; then
+  optipng "${imgPath}" || \
+  osascript -e 'display notification "Something went wrong" with title "Optimisation failed"'
+elif [[ "$imgExt" == .[gG][iI][fF] ]]; then
+  gifsicle --colors 256 -O3 "${imgPath}" -o "${imgPath}" || \
+  osascript -e 'display notification "Something went wrong" with title "Optimisation failed"'
+fi
+
+tag -a "${imgTags}" "${imgPath}" || osascript -e 'display notification "Something went wrong" with title "Tagging failed"'
 
 osascript -e 'on run {f, c}' -e 'tell app "Finder" to set comment of (POSIX file f as alias) to c' -e end "${imgPath}" "${imgComment}"
