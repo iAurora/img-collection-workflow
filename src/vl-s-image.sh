d34a1bbb741cb00692d9cf92a7b3f8779482809d
Valeria K <laaurora.val@gmail.com>
2016-08-11 04:52:14 +0300

    Simplify filenames. Fix optimisation bug for page screenshots. Minor cleanup.

--- src/vl-s-image.sh
+++ src/vl-s-image.sh
@@ -5,9 +5,9 @@ dir="${BASH_SOURCE%/*}"
 if [[ ! -d "$dir" ]]; then dir="$PWD"; fi
 . "$dir/vl-config.sh"
 
-
 # Make sure all checks below are case-insensitive
 shopt -s nocasematch
+
 
 # Get the URL from the clipboard
 resourceURL="$(pbpaste)"
@@ -17,8 +17,7 @@ if [[ $resourceURL = *?dl=* ]]; then
   resourceURL="${resourceURL%?dl=*}"
 fi
 
-
-# Check the URL
+# Make sure that $resourceURL contains an URL with an accepted file extension
 checkPattern="^(https?|ftp|file).+(\.($acceptedFileTypes)+)$"
 
 if [[ ! $resourceURL =~ $checkPattern ]]; then 
@@ -27,12 +26,6 @@ if [[ ! $resourceURL =~ $checkPattern ]]; then
 fi
 
 
-# Get the URL of the frontmost tab in Safari
-pageURL="$(osascript -e 'tell application "Safari" to set pageURL to URL of front document')"
-
-# Get the title of the frontmost tab of Safari
-pageTitle="$(osascript -e 'tell application "Safari" to set pageTitle to name of front document')"
-
 # Get the extention of the linked image
 fileExtension=".${resourceURL##*.}"
 
@@ -40,36 +33,28 @@ fileExtension=".${resourceURL##*.}"
 timeStamp=$(date '+%s')
 
 
-# Get provided tags or set the default one
-if [[ -n $2 ]]; then
-  macTags="$2"
-else
-  macTags="untagged"
-fi
-
-
 # Pick the title for the file (custom from arg 3/4 or default)
 if [[ ${#3} -gt 3 && ${#4} -le 3 && -z $5 ]]; then
-  fileTitle="$3"
+  fileTitle="${3} ${timeStamp}"
 
 elif [[ ${#4} -gt 3 && ${#3} -le 3 && -z $5 ]]; then
-  fileTitle="$4"
+  fileTitle="${4} ${timeStamp}"
 
 elif [[ ${#3} -gt 3 && ${#4} -gt 3 || ${#3} -ge 1 && ${#3} -le 3 && ${#4} -ge 1 && ${#4} -le 3 || $5 ]]; then
-  fileTitle="${pageTitle}"
-  osascript -e 'display notification "The provided custom title doesn'"'"'t look right. Using the frontmost browser tab title instead." with title "Ooops!"'
+  fileTitle="${timeStamp}"
+  osascript -e 'display notification "The provided custom title doesn'"'"'t look right. Using the default title instead." with title "Ooops!"'
 
-else fileTitle="${pageTitle}"
+else fileTitle="${timeStamp}"
 
 fi
 
 
-# Compose the filename (+ retina handling)
+# Compose the filename with retina option
 if [[ "$3" =~ (r|2x|@2x) || "$4" =~ (r|2x|@2x) ]]; then
-  fileName="${fileTitle} ${timeStamp} @2x${fileExtension}"
+  fileName="${fileTitle} @2x${fileExtension}"
 
 else 
-  fileName="${fileTitle} ${timeStamp}${fileExtension}"
+  fileName="${fileTitle}${fileExtension}"
   
 fi
 
@@ -81,7 +66,7 @@ filePath=$rootDir$subDir$fileName
 curl -o "${filePath}" "${resourceURL}"
 
 
-# Check the success status
+# Check the download success status
 if [[ $? -eq 1 ]]; then
  osascript -e 'delay "0.5"' -e 'display notification "Something is wrong with the file you are trying to download. Make sure the link is valid" with title "Failure!"'
  exit 1
@@ -91,7 +76,9 @@ fi
 # Optimize the image if the type is jpg, png or gif
 if $optimiseImages; then 
 
-  optimisationError () { osascript -e 'delay "0.5"' -e 'display notification "Something went wrong" with title "Optimisation failed"'; }
+  optimisationError () { 
+    osascript -e 'delay "0.5"' -e 'display notification "Something went wrong" with title "Optimisation failed"' 
+  }
 
   if [[ "$fileExtension" == .jp*g ]]; then optimiseJPG
 
@@ -104,14 +91,29 @@ if $optimiseImages; then
 fi
 
 
+# Get provided tags or set the default one
+if [[ -n $2 ]]; then
+  macTags="$2"
+else
+  macTags="untagged"
+fi
+
 # Apply macOS tags to the file
 /usr/local/bin/tag -a "${macTags}" "${filePath}" || osascript -e 'delay "0.5"' -e 'display notification "Something went wrong" with title "Tagging failed"'
 
-# Compose the Finder comment
-imgComment="title: ${pageTitle}"$'\n\n'"page: ${pageURL}"$'\n\n'"url: ${resourceURL}"
 
-# Apply the Finder comment
-osascript -e 'on run {f, c}' -e 'tell app "Finder" to set comment of (POSIX file f as alias) to c' -e end "${filePath}" "${imgComment}"
+# Get the URL of the frontmost tab in Safari
+pageURL="$(osascript -e 'tell application "Safari" to set pageURL to URL of front document')"
+
+# Get the title of the frontmost tab of Safari
+pageTitle="$(osascript -e 'tell application "Safari" to set pageTitle to name of front document')"
+
+# Compose the Finder Comments
+finderComments="title: ${pageTitle}"$'\n\n'"page: ${pageURL}"$'\n\n'"url: ${resourceURL}"
+
+# Apply the Finder Comments
+osascript -e 'on run {f, c}' -e 'tell app "Finder" to set comment of (POSIX file f as alias) to c' -e end "${filePath}" "${finderComments}"
+
 
 # Confirm success
 osascript -e 'delay "1"' -e 'display notification "All went well. Hopefully." with title "Job complete!"'
--- src/vl-ss-area.sh
+++ src/vl-ss-area.sh
@@ -5,12 +5,9 @@ dir="${BASH_SOURCE%/*}"
 if [[ ! -d "$dir" ]]; then dir="$PWD"; fi
 . "$dir/vl-config.sh"
 
+# Make sure all checks below are case-insensitive
+shopt -s nocasematch
 
-# Get the URL of the frontmost tab in Safari
-pageURL="$(osascript -e 'tell application "Safari" to set pageURL to URL of front document')"
-
-# Get the title of the frontmost tab of Safari
-pageTitle="$(osascript -e 'tell application "Safari" to set pageTitle to name of front document')"
 
 # Generate the timestamp
 timeStamp=$(date '+%s')
@@ -19,36 +16,28 @@ timeStamp=$(date '+%s')
 fileExtension=".png"
 
 
-# Get provided tags or set the default one
-if [[ -n $2 ]]; then
-  macTags="$2"
-else
-  macTags="untagged"
-fi
-
-
 # Pick the title for the file (custom from arg 3/4 or default)
 if [[ ${#3} -gt 3 && ${#4} -le 3 && -z $5 ]]; then
-  fileTitle="$3"
+  fileTitle="${3} ${timeStamp}"
 
 elif [[ ${#4} -gt 3 && ${#3} -le 3 && -z $5 ]]; then
-  fileTitle="$4"
+  fileTitle="${4} ${timeStamp}"
 
 elif [[ ${#3} -gt 3 && ${#4} -gt 3 || ${#3} -ge 1 && ${#3} -le 3 && ${#4} -ge 1 && ${#4} -le 3 || $5 ]]; then
-  fileTitle="${pageTitle}"
-  osascript -e 'display notification "The provided custom title doesn'"'"'t look right. Using the frontmost browser tab title instead." with title "Ooops!"'
+  fileTitle="${timeStamp}"
+  osascript -e 'display notification "The provided custom title doesn'"'"'t look right. Using the default title instead." with title "Ooops!"'
 
-else fileTitle="${pageTitle}"
+else fileTitle="${timeStamp}"
 
 fi
 
 
-# Compose the filename (+ retina handling)
+# Compose the filename with retina option
 if [[ "$3" =~ (r|2x|@2x) || "$4" =~ (r|2x|@2x) ]]; then
-  fileName="${fileTitle} ${timeStamp} @2x${fileExtension}"
+  fileName="${fileTitle} @2x${fileExtension}"
 
 else 
-  fileName="${fileTitle} ${timeStamp}${fileExtension}"
+  fileName="${fileTitle}${fileExtension}"
   
 fi
 
@@ -63,21 +52,38 @@ screencapture -io "${filePath}"
 # Optimize the image
 if $optimiseImages; then 
 
-  optimisationError () { osascript -e 'delay "0.5"' -e 'display notification "Something went wrong" with title "Optimisation failed"'; }
+  optimisationError () { 
+    osascript -e 'delay "0.5"' -e 'display notification "Something went wrong" with title "Optimisation failed"' 
+  }
 
   optimisePNG
 
 fi
 
 
+# Get provided tags or set the default one
+if [[ -n $2 ]]; then
+  macTags="$2"
+else
+  macTags="untagged"
+fi
+
 # Apply macOS tags to the file
 /usr/local/bin/tag -a "${macTags}" "${filePath}" || osascript -e 'delay "0.5"' -e 'display notification "Something went wrong" with title "Tagging failed"'
 
-# Compose the Finder comment
-imgComment="title: ${pageTitle}"$'\n\n'"page: ${pageURL}"
 
-# Apply the Finder comment
-osascript -e 'on run {f, c}' -e 'tell app "Finder" to set comment of (POSIX file f as alias) to c' -e end "${filePath}" "${imgComment}"
+# Get the URL of the frontmost tab in Safari
+pageURL="$(osascript -e 'tell application "Safari" to set pageURL to URL of front document')"
+
+# Get the title of the frontmost tab of Safari
+pageTitle="$(osascript -e 'tell application "Safari" to set pageTitle to name of front document')"
+
+# Compose the Finder Comments
+finderComments="title: ${pageTitle}"$'\n\n'"page: ${pageURL}"
+
+# Apply the Finder Comments
+osascript -e 'on run {f, c}' -e 'tell app "Finder" to set comment of (POSIX file f as alias) to c' -e end "${filePath}" "${finderComments}"
+
 
 # Confirm success
 osascript -e 'display notification "All went well. Hopefully." with title "Job complete!"'
